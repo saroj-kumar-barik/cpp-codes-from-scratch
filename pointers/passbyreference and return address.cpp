541c00b4692705a91470a5cda775e71dea117fb0
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2021-06-01 17:46:13 +0530

    pass by reference and return reference (address)

---
+++ pointers/passbyreference and return address.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int& finding_largest_among_two(int &p, int &q);
+int main(){
+    int a,b;
+    cout<<"Enter two numbers A & B : ";
+    cin>>a>>b;
+    int &res = finding_largest_among_two(a, b);
+    if (*(&res) == a)
+        cout<<"Largest num is A = "<<res<<endl;
+    else
+        cout<<"Largest num is B = "<<res<<endl;   
+        
+}
+                            //  1000     2000 
+int &finding_largest_among_two(int &p, int &q){
+    if(*(&p) > *(&q))
+        return p;
+    else
+        return q;
+}
